d0c3256cf10c884a010b03c5ffd07e1f113d59f8
DennyCai <545965765@qq.com>
2015-08-05 11:00:48 +0800

    support Minimal SDK Version 8

--- app/src/main/java/com/github/glomadrian/codeinput/MainActivity.java
+++ app/src/main/java/com/github/glomadrian/codeinput/MainActivity.java
@@ -1,9 +1,9 @@
 package com.github.glomadrian.codeinput;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
